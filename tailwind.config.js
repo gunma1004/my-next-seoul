2e0dd291b37a7cc4882a124289f0ba49e508dbea
gunma1004 <hanabank94@naver.com>
2026-07-20 14:03:40 +0900

    Tailwind CSS content 경로 만능으로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // ⭐️ 가장 중요한 부분! app 폴더 안의 디자인을 읽어오라는 뜻입니다.
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // src 폴더 안팎을 모두 샅샅이 뒤집니다!
   ],
   theme: {
     extend: {
